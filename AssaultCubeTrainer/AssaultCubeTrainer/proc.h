3e91286be667121e23e926f9b0ac6d2f6fe3cfa3
Conner Schiissler <conner.schiissler@gmail.com>
2020-07-05 19:56:03 -0600

    First working version to change ammo value

--- AssaultCubeTrainer/AssaultCubeTrainer/proc.h
+++ AssaultCubeTrainer/AssaultCubeTrainer/proc.h
@@ -6,5 +6,5 @@ DWORD GetProcId(const wchar_t* procName);
 
 uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName);
 
-uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t, std::vector<unsigned int>);
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets);
 
